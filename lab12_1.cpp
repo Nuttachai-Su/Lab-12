5b9a4b36bad10eeb72b7bd50ef94d54065aef6d7
Nuttachai-Su <lasernaball@gmail.com>
2025-12-30 15:42:44 +0700

    Complete1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int i){
+    int sum = 0;
+    if(i == 0) sum = 0;
+    else if (i == 1) sum = 1;
+    else if (i > 1) {
+        sum += fibonacci(i-1) + fibonacci(i-2);
+    }
+
+    return sum;
 }
